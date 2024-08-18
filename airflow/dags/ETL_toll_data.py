c1540ca33d58993905a303e9f8d695103a2c4200
SOUlEYMANEFOMBA <souleymanefmb@gmail.com>
2024-08-18 22:45:36 +0200

    feat : add untar_dataset function

--- airflow/dags/ETL_toll_data.py
+++ airflow/dags/ETL_toll_data.py
@@ -2,13 +2,14 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 import requests
-import tarfile
+import tarfile as tar
 
 def download_dataset():
+    
     url =" https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Final%20Assignment/tolldata.tgz"
     
     ##Chemin de destination pour le fichier tar télecharger
-    destination_tar="C:\Users\soule\Desktop\ETL\Build-ETL-Data-Pipelines-with-PythonOperator-using-Apache-Airflow\airflow\data"
+    destination_tar="C:\Users\soule\Desktop\ETL\Build-ETL-Data-Pipelines-with-PythonOperator-using-Apache-Airflow\airflow\data_tar"
     
     #Télechargement du fichier tar 
     response=requests.get(url)
@@ -16,9 +17,16 @@ def download_dataset():
     with open(destination_tar,"wb") as file:
         file.write(response.content)
     
-
 def untar_dataset():
-    pass
+    
+    ##Chemin de destination pour le fichier tar télecharger
+    destination_tar="C:\Users\soule\Desktop\ETL\Build-ETL-Data-Pipelines-with-PythonOperator-using-Apache-Airflow\airflow\data_tar"
+    #chemin pour untar le fichier tar 
+    extract_folder="C:\Users\soule\Desktop\ETL\Build-ETL-Data-Pipelines-with-PythonOperator-using-Apache-Airflow\airflow\extract_foder"
+    
+    ##untar dataset
+    with open(destination_tar,'r') as file :
+        tar.extractall(path=extract_folder)
 
 def extract_data_from_csv():
     pass
