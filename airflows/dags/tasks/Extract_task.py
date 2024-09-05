011041aca0f1ec246f4c25dc0d716f63ec5b8d1a
SOUlEYMANEFOMBA <souleymanefmb@gmail.com>
2024-09-05 11:30:47 +0000

    feat: add new fonction consilidate

--- airflows/dags/tasks/Extract_task.py
+++ airflows/dags/tasks/Extract_task.py
@@ -1,13 +1,15 @@
 import os
 import pandas as pd 
 import csv
+import sys
 
 class ExtractTask():
     
     def extract_data_from_csv(self,csv_file,extract_folder):
-                print("begining of cvs extrac file")
+                print("begining of csv extrac file")
                 if not os.path.exists(extract_folder):
-                    os.makedirs(extract_folder)
+                    print("Folder doesn't exist, untardata probably fail")
+                    sys.exit(1)
                 #Easy methode
                 # vehicle_data_csv=pd.read_csv(csv_file,sep=",")
                 # variables=["Rowid", "Timestamp", "Anonymized Vehicle number", "Vehicle type"]
@@ -28,8 +30,10 @@ class ExtractTask():
                     print(f"An error occured: {e}")
 
     def extract_data_from_tsv(self,tsv_file, extract_folder):
+            print("begining of tsv extrac file")
             if not os.path.exists(extract_folder):
-                os.makedirs(extract_folder)
+                print("Folder doesn't exist, untardata probably fail")
+                sys.exit(1)
             output_file=f"{extract_folder}/tsv_data.csv"
             try :
                 with open(tsv_file,'r') as infile, open(output_file,'w') as outfile:
@@ -44,8 +48,10 @@ class ExtractTask():
         
 
     def extract_data_from_fixed_width(self,payement_data_txt, extract_folder):
+                print("begining of fised_data extrac file")
                 if not os.path.exists(extract_folder):
                     print("Folder doesn't exist, untardata probably fail")
+                    sys.exit(1)
                 output_file=f"{extract_folder}/fixed_width_data.csv"
                 try :
                     with open(payement_data_txt,'w') as infile, open(output_file,'w') as outfile:
@@ -56,5 +62,27 @@ class ExtractTask():
                     print(f"extract_data_from_fixed_width extracted and saved to {output_file}")
                 except Exception as e :
                     print(f"An error occured: {e}")
-              
-    
+                
+    def consolidate_data(self,csv_data_file,tsv_data_file,fixed_width_data_file,extract_folder):
+                print("begining of consolidate data")
+                if not os.path.exists(extract_folder):
+                    print("folder doesn't exist, untardata probably fail")
+                    sys.exit(1)
+                
+                output_file = f"{extract_folder}/extracted_data.csv"
+                try :
+                    with open(csv_data_file,'r') as csvfile, open(tsv_data_file,'r') as tsvfile, open(fixed_width_data_file,'r') as fixedfile, open(output_file,'w') as outfile:
+                        csv_reader = csv.reader(csvfile)
+                        tsv_reader = csv.reader(tsvfile)
+                        fixed_reader = csv.reader(fixedfile)
+                        writer=csv.writer(outfile)
+                        writer.writerow(["Rowid", "Timestamp", "Anonymized Vehicle number", "Vehicle type", "Number of axles", "Tollplaza id", "Tollplaza code", "Type of Payment code", "Vehicle Code"])
+                        next(csv_reader)
+                        next(tsv_reader)
+                        next(fixed_reader)
+                        for csv_row,tsv_row,fixed_row in zip(csv_reader,tsv_reader,fixed_reader):
+                            writer.writerow(csv_row,tsv_row,fixed_row)
+                    print(f"Data consolidated and saved to {output_file}")
+                except Exception as e:
+                    print(f"An error occured : {e}")               
+                
